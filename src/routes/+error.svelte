3f01f7f8433ef666172b1ad988386a0831b5216a
LeviOP <l3viop@gmail.com>
2023-04-11 16:25:01 -0500

    start navbar

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,5 +1,5 @@
-<div class="flex flex-col h-screen w-screen text-w text-center justify-center">
-    <b class="text-8xl">Error</b>
-    <i class="text-2xl">How did you get here?</i>
-    <button on:click={() => history.back()} class="text-w rounded bg-active p-1 w-fit mx-auto mt-2">Go back...</button>
+<div class="flex flex-col h-screen w-screen text-w text-center justify-center items-center">
+    <strong class="text-8xl">Error</strong>
+    <em class="text-2xl">How did you get here?</em>
+    <button on:click={() => history.back()} class="text-w rounded bg-active p-1 w-fit mt-2">Go back...</button>
 </div>
